b777d76a4119b4ae1f2bf158f60a4f6630cebb1c
Teja8899 <99643609+Teja8899@users.noreply.github.com>
2022-04-23 10:23:35 +0530

    #include<stdio.h> int main() {     int v,k,r;     scanf("%d",&r);     for(v=1;v<=r;v++)     {         for(k=1;k<=r;k++)         {             if(v+k==r+1 || v-k==0)             {                 printf("x");             }             else             {                 printf("0");             }         }         printf(" ");     }     return 0; }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int v,k,r;
+    scanf("%d",&r);
+    for(v=1;v<=r;v++)
+    {
+        for(k=1;k<=r;k++)
+        {
+            if(v+k==r+1 || v-k==0)
+            {
+                printf("x");
+            }
+            else
+            {
+                printf("0");
+            }
+        }
+        printf("
+");
+    }
+    return 0;
+}
