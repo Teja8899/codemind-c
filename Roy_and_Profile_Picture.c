79240ee69348428873c7fd34b820b98b100ed2db
Teja8899 <99643609+Teja8899@users.noreply.github.com>
2022-05-14 22:09:21 +0530

    #include<stdio.h> int main() {     int l,n,v,k,i;     scanf("%d %d",&l,&n);     for(i=1;i<=n;i++)     {         scanf("%d %d ",&v,&k);       if(v<l || k<l)       {           printf("UPLOAD ANOTHER ");       }       else if(v>=1 && k>=1)       {           if(v==k)           {               printf("ACCEPTED ");           }           else           {               printf("CROP IT ");           }       }     }     return 0; }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int l,n,v,k,i;
+    scanf("%d
+%d",&l,&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d %d
+",&v,&k);
+      if(v<l || k<l)
+      {
+          printf("UPLOAD ANOTHER
+");
+      }
+      else if(v>=1 && k>=1)
+      {
+          if(v==k)
+          {
+              printf("ACCEPTED
+");
+          }
+          else
+          {
+              printf("CROP IT
+");
+          }
+      }
+    }
+    return 0;
+}
