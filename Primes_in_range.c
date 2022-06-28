44e495186253487c9ba5b569fe2bc90196e307b3
ROTTE BALA SURYA VENKATA TEJA <99643609+Teja8899@users.noreply.github.com>
2022-06-28 10:56:39 +0530

    #include<stdio.h> #include<math.h> int main() {     int v,k,r,n,l=0,j;     scanf("%d %d",&r,&v);     for(k=r;k<=v;k++)     {         n=0;         if(k==1)         {             continue;         }         for(j=2;j<=sqrt(k);j++)         {             if(k%j==0)             {                 n++;                 break;             }         }         if(n==0)         {             l++;         }     }     printf("%d",l); }

---
+++ Primes_in_range.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int v,k,r,n,l=0,j;
+    scanf("%d %d",&r,&v);
+    for(k=r;k<=v;k++)
+    {
+        n=0;
+        if(k==1)
+        {
+            continue;
+        }
+        for(j=2;j<=sqrt(k);j++)
+        {
+            if(k%j==0)
+            {
+                n++;
+                break;
+            }
+        }
+        if(n==0)
+        {
+            l++;
+        }
+    }
+    printf("%d",l);
+}
